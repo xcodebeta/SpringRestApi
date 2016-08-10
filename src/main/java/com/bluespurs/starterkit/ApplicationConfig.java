dd665b9955569fc12b0582f035dc4da76dd3bc38
Howard Powell <howard.powell@bluespurs.com>
2016-08-10 08:45:52 -0300

    Updates to unit tests

--- src/main/java/com/bluespurs/starterkit/ApplicationConfig.java
+++ src/main/java/com/bluespurs/starterkit/ApplicationConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- src/test/java/com/bluespurs/starterkit/UnitTest.java
+++ src/test/java/com/bluespurs/starterkit/UnitTest.java
@@ -3,12 +3,12 @@ package com.bluespurs.starterkit;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UnitTestConfig.class)
+@SpringBootTest(classes = UnitTestConfig.class)
 @WebAppConfiguration
 public abstract class UnitTest {
     @Before
